412e678a47ec36f95882ab8e766b8483762a320e
YuriS <yuri-s@list.ru>
2024-02-23 23:04:32 +0300

    New tests

--- src/main/java/MovieManager.java
+++ src/main/java/MovieManager.java
@@ -1,2 +1,43 @@
 public class MovieManager {
+
+    private String[] movies = new String[0];    // Мфссив фильмов
+    private int limit;                          // Создаем поле для ввода параметра limit
+
+    public MovieManager() {                     // Конструктор без параметров
+        this.limit = 5;     //Устанавливаем limit для вывода кол-ва  последних введенных фильмов  (5 по умолчанию),
+    }
+
+    public MovieManager(int limit) { // Конструктор c параьетром limit для вывода последних с параметром (кол-вом фильм.)
+        this.limit = limit;          // Присваиваем значение тек. параметру равное limimt
+    }
+    public void addMovie(String movie) {                 //  Метод добавления фильмов в массив
+        String[] tmp = new String[movies.length + 1];    // Создаем массив на 1 больше исходного
+        for (int i = 0; i < movies.length; i++) {       // Перебараем данные массива
+            tmp[i] = movies[i];                         // Находим нужный элемент
+        }
+        tmp[tmp.length - 1] = movie;            // Меняем длину массива
+        this.movies = tmp;                      // Текущему фильму присваиваеи номер в массиве
+    }
+    public String[] findAll() {                 // Метод вывода полного списка фильмов
+        return movies;
+    }
+
+
+
+
+    public String[] findLast() {        // Метод вывода списка последних ( 5-ти по умолчанию, или указанное в параметре)
+        // в обратном порядке
+        int resultLangth;
+
+        if (movies.length < limit) {        // Подсчет кол-ва фильмов, если число выбранных для просмотра
+            resultLangth = movies.length;   // больше, чем  введено фильмов
+        } else {
+            resultLangth = limit;
+        }
+        String[] tmp = new String[resultLangth];    // Метод подсчета числа последних фильмов из массива
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = movies[movies.length -1 - i];
+        }
+        return tmp;
+    }
 }
--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -1,2 +1,145 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MovieManagerTest {
+
+    @Test
+    public void amptyMovieManager() {
+        MovieManager manager = new MovieManager(0);
+        String[] actual = manager.findAll();
+        String[] expected = {};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void Add_1Movies() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film AA");
+        String[] actual = manager.findAll();
+        String[] expected = {"Film AA"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void Add_5Movies() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film AA");
+        manager.addMovie("Film BB");
+        manager.addMovie("Film CC");
+        manager.addMovie("Film DD");
+        manager.addMovie("Film EE");
+        String[] actual = manager.findAll();
+        String[] expected = {"Film AA", "Film BB", "Film CC", "Film DD", "Film EE"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void Add_7Movies() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film AA");
+        manager.addMovie("Film BB");
+        manager.addMovie("Film CC");
+        manager.addMovie("Film DD");
+        manager.addMovie("Film EE");
+        manager.addMovie("Film FF");
+        manager.addMovie("Film GG");
+        String[] actual = manager.findAll();
+        String[] expected = {"Film AA", "Film BB", "Film CC", "Film DD", "Film EE", "Film FF", "Film GG"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findAll() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film AA");
+        manager.addMovie("Film BB");
+        manager.addMovie("Film CC");
+        manager.addMovie("Film DD");
+        manager.addMovie("Film EE");
+        manager.addMovie("Film FF");
+        manager.addMovie("Film GG");
+        manager.addMovie("Film HH");
+        manager.addMovie("Film QQ");
+        String[] actual = manager.findAll();
+        String[] expected = {"Film AA", "Film BB", "Film CC", "Film DD", "Film EE",
+                "Film FF", "Film GG", "Film HH", "Film QQ"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastWithoutParam() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Film AA");
+        manager.addMovie("Film BB");
+        manager.addMovie("Film CC");
+        manager.addMovie("Film DD");
+
+        manager.addMovie("Film EE");
+        manager.addMovie("Film FF");
+        manager.addMovie("Film GG");
+        manager.addMovie("Film HH");
+        manager.addMovie("Film QQ");
+        String[] actual = manager.findLast();
+        String[] expected = {"Film QQ", "Film HH", "Film GG", "Film FF", "Film EE"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLast1Film() {
+        MovieManager manager = new MovieManager(1);
+        manager.addMovie("Film AA");
+        manager.addMovie("Film BB");
+        manager.addMovie("Film CC");
+        manager.addMovie("Film DD");
+        manager.addMovie("Film EE");
+        manager.addMovie("Film FF");
+        manager.addMovie("Film GG");
+        manager.addMovie("Film HH");
+
+        manager.addMovie("Film QQ");
+        String[] actual = manager.findLast();
+        String[] expected = {"Film QQ"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void findLast7Films() {
+        MovieManager manager = new MovieManager(7);
+
+        manager.addMovie("Film AA");
+        manager.addMovie("Film BB");
+
+        manager.addMovie("Film CC");
+        manager.addMovie("Film DD");
+        manager.addMovie("Film EE");
+        manager.addMovie("Film FF");
+        manager.addMovie("Film GG");
+        manager.addMovie("Film HH");
+        manager.addMovie("Film QQ");
+        String[] actual = manager.findLast();
+        String[] expected = {"Film QQ", "Film HH", "Film GG", "Film FF", "Film EE",
+                "Film DD", "Film CC"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLast10Films() {
+        MovieManager manager = new MovieManager(10);
+
+        manager.addMovie("Film AA");
+        manager.addMovie("Film BB");
+        manager.addMovie("Film CC");
+        manager.addMovie("Film DD");
+        manager.addMovie("Film EE");
+        manager.addMovie("Film FF");
+        manager.addMovie("Film GG");
+        manager.addMovie("Film HH");
+        manager.addMovie("Film QQ");
+        String[] actual = manager.findLast();
+        String[] expected = {"Film QQ", "Film HH", "Film GG", "Film FF", "Film EE",
+                "Film DD", "Film CC", "Film BB", "Film AA"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
